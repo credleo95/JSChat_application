24edcdf1198550cadd2e466cf068d3d814065c29
Omarius <70237021+credleo95@users.noreply.github.com>
2022-01-15 01:40:44 -0500

    handled logic for both my message and their message.

--- src/components/ChatFeed.js
+++ src/components/ChatFeed.js
@@ -8,11 +8,8 @@ function ChatFeed(props) {
 
   const chat = chats && chats[activeChat];
 
-  console.log(chat, username, messages);
-
   const fetchMessages = () => {
     const keys = Object.keys(messages);
-    console.log(keys);
     return keys.map((key, index) => {
       const message = messages[key];
       const lastMessageKey = index === 0 ? null : keys[index - 1];
@@ -20,7 +17,14 @@ function ChatFeed(props) {
       return (
         <div key={`msg_${index}`} style={{ width: '100%' }}>
           <div className="message-block">
-            {isMyMessage ? <MyMessage /> : <TheirMessage />}
+            {isMyMessage ? (
+              <MyMessage message={message} />
+            ) : (
+              <TheirMessage
+                message={message}
+                lastMessage={messages[lastMessageKey]}
+              />
+            )}
           </div>
           <div
             className="read-receipts"
@@ -37,8 +41,19 @@ function ChatFeed(props) {
   };
 
   return (
-    <div>
-      <p>ChatFeed!</p>
+    <div className="chat-feed">
+      <div className="chat-title-container">
+        <div className="chat-title"> {chat.title}</div>
+        <div className="chat-subtitle">
+          {chat.people.map((person) => `${person.person.username}`)}
+        </div>
+      </div>
+
+      {fetchMessages()}
+      <div style={{ height: '100px' }} />
+      <div className="message-form-container">
+        <MessageForm {...props} chatId={activeChat} />
+      </div>
     </div>
   );
 }
